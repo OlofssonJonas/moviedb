96833940f5dfb5f5a00c00b38a77bad6ebbcd9fb
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-05-16 20:18:26 +0200

    fetch

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -4,7 +4,7 @@ import MarkAsSeenBtn from '../MarkAsSeenBtn/MarkAsSeenBtn'
 function MovieDetails() {
   return (
     <div>
-    {/* fetch this https://api.themoviedb.org/3/movie/550?api_key=b59bf13feefaa44767fe3024e9db5376 */}
+      <h2>Hello Movielist</h2>
         <MarkAsSeenBtn />
     </div>
   )
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import FilterDropdown from '../FilterDropdown/FilterDropdown'
 import MovieCard from '../MovieCard/MovieCard'
+import Axios from 'axios'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 
 function MovieList() {
-  return (
-    <div>
-        <FilterDropdown />
-        <MovieCard />
+  const [movie, setMovie] = useState([]);
+
+  useEffect (() => {
+
+    Axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=b59bf13feefaa44767fe3024e9db5376')
+    .then((res) => {
+      console.log(res.data.results)
+      setMovie(res.data.results);
+  })
+  .catch ((error) => {
+    console.log('Error')
+  })
+  
+  }, []);
+
+    return (
+      <>
+        <div>
+       {movie.map((movieInfo) => (
+        //console.log(movieInfo)
+         <h3 key={movieInfo.id}>{movieInfo.title}</h3>
+         ))},
+        </div>,
+        <div>
+         <FilterDropdown />
+         <MovieCard /> 
     </div>
+         </>
   )
 }
 
